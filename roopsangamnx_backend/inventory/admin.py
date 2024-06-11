ef44902f9cc80db542d4ca08be3a7161ca78266d
saichothave <chothavesai@gmail.com>
2024-06-12 01:27:06 +0530

    updating gitignore to avoid cache

--- roopsangamnx_backend/inventory/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product, Brand, SubCategory
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Brand)
-admin.site.register(SubCategory)
-
