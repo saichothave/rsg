31e1ddff1273165c9384533930fc6c8d4d61bc3f
saichothave <chothavesai@gmail.com>
2024-06-12 21:21:36 +0530

    updated vercel and build sh

--- roopsangamnx_backend/build_files.sh
+++ roopsangamnx_backend/build_files.sh
@@ -1,6 +1,5 @@
 echo "Build Start"
-source ../rsenv/bin/activate
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
 echo "Build End"
 
