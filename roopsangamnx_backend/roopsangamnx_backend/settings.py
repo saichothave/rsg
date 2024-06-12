1e96b3eec9dfe163866a9d0b26add6e885b89e90
saichothave <chothavesai@gmail.com>
2024-06-12 21:44:17 +0530

    added psql db

--- roopsangamnx_backend/roopsangamnx_backend/settings.py
+++ roopsangamnx_backend/roopsangamnx_backend/settings.py
@@ -117,8 +117,12 @@ print('MEDIA_ROOT', MEDIA_ROOT)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'rs_db',
+    'USER': 'postgres',
+    'PASSWORD': 'postgres',
+    'HOST': 'localhost',
+    'PORT': '5432', 
     }
 }
 
