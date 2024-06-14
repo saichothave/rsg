5d48f4afe36ee90ad6115bc21abb8d4ddb8aa5b1
Saurabh Mahajan <msaurabh50702@gmail.com>
2024-06-14 17:31:20 +0530

    static files issue resolved

--- roopsangamnx_backend/roopsangamnx_backend/settings.py
+++ roopsangamnx_backend/roopsangamnx_backend/settings.py
@@ -109,7 +109,8 @@ WSGI_APPLICATION = 'roopsangamnx_backend.wsgi.application'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'gallery/'),)
+
 
 # Media files
 MEDIA_URL = '/media/'
