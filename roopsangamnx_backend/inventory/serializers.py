bb92c30b2744abc8e9dddf66e13bdfaac6b17c00
saichothave <chothavesai@gmail.com>
2024-06-16 13:26:32 +0530

    updated serializer

--- roopsangamnx_backend/inventory/serializers.py
+++ roopsangamnx_backend/inventory/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Product, SubCategory
+from .models import Category, Product, SubCategory, Brand, ProductColor, ProductSize
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,10 +15,28 @@ class SubcategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+class ProductColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductColor
+        fields = '__all__'
+
+class ProductSizeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductSize
+        fields = '__all__'
+
+
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     subcategory = SubcategorySerializer()
-
+    brand = BrandSerializer()
+    size = ProductSizeSerializer()
+    color = ProductColorSerializer()
     class Meta:
         model = Product
         fields = '__all__'
