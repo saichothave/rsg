fe0220c950930e10c8b59118b3326fab84349121
Saurabh Mahajan <msaurabh50702@gmail.com>
2024-06-19 13:57:17 +0530

    Created seperate serializer to handle post request on product api

--- roopsangamnx_backend/inventory/serializers.py
+++ roopsangamnx_backend/inventory/serializers.py
@@ -40,3 +40,82 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    # def create(self, validated_data):
+    #     return Product.objects.create(**validated_data)
+
+    # def create(self, validated_data):
+    #     print("From Serl")
+    #     brand_data = validated_data.pop('brand')
+    #     category_data = validated_data.pop('category')
+    #     subcategory_data = validated_data.pop('subcategory', None)
+    #     size_data = validated_data.pop('size', None)
+    #     color_data = validated_data.pop('color', None)
+
+    #     brand, created = Brand.objects.get_or_create(name=brand_data['name'])
+    #     category, created = Category.objects.get_or_create(name=category_data['name'])
+        
+    #     subcategory = None
+    #     if subcategory_data:
+    #         subcategory, created = SubCategory.objects.get_or_create(
+    #             name=subcategory_data['name'],
+    #             defaults={'category': category}
+    #         )
+
+    #     size = None
+    #     if size_data:
+    #         size, created = ProductSize.objects.get_or_create(size=size_data['size'])
+
+    #     color = None
+    #     if color_data:
+    #         color, created = ProductColor.objects.get_or_create(color=color_data['color'])
+
+    #     print("OKOKOK")
+
+    #     product = Product.objects.create(
+    #         brand=brand,
+    #         category=category,
+    #         subcategory=subcategory,
+    #         size=size,
+    #         color=color,
+    #         **validated_data
+    #     )
+    #     return product
+    
+    # # def create(self, validated_data):
+    #     brand_data = validated_data.pop('brand')
+    #     category_data = validated_data.pop('category')
+    #     subcategory_data = validated_data.pop('subcategory')
+    #     size_data = validated_data.pop('size')
+    #     color_data = validated_data.pop('color')
+
+    #     brand = Brand.objects.create(**brand_data)
+    #     category = Category.objects.create(**category_data)
+    #     subcategory = Subcategory.objects.create(**subcategory_data)
+    #     size = Size.objects.create(**size_data)
+    #     color = Color.objects.create(**color_data)
+
+    #     product = Product.objects.create(
+    #         brand=brand,
+    #         category=category,
+    #         subcategory=subcategory,
+    #         size=size,
+    #         color=color,
+    #         **validated_data
+    #     )
+
+    #     return product
+
+    
+class ProductWriteSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    subcategory = serializers.PrimaryKeyRelatedField(queryset=SubCategory.objects.all(), required=False, allow_null=True)
+    brand = serializers.PrimaryKeyRelatedField(queryset=Brand.objects.all())
+    size = serializers.PrimaryKeyRelatedField(queryset=ProductSize.objects.all(), required=False, allow_null=True)
+    color = serializers.PrimaryKeyRelatedField(queryset=ProductColor.objects.all(), required=False, allow_null=True)
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data) 
