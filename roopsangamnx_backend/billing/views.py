0524802798faabb4aa6725e06a001603c2b13ccc
msaurabh50702 <msaurabh50702@gmail.com>
2024-07-05 21:45:11 +0530

    Implemented logout functionality and endpoints to populate fields on front end

--- roopsangamnx_backend/billing/views.py
+++ roopsangamnx_backend/billing/views.py
@@ -1,13 +1,16 @@
+from django.db.models import Sum
 from rest_framework import viewsets
 from authentication.permissions import IsShopOwner, IsBillingDesk
 from .invoice import generate_invoice_image
 from .models import Customer, BillingDesk, Billing, BillingItem
-from .serializers import CustomerSerializer, BillingDeskSerializer, BillingSerializer, BillingItemSerializer
+from .serializers import BDDashBoardSerializer, CustomerSerializer, BillingDeskSerializer, BillingSerializer, BillingItemSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db import transaction
 from rest_framework import status
 from rest_framework import generics, permissions
+
+from django.utils import timezone
 
 
 
@@ -38,3 +41,24 @@ class BillingViewSet(viewsets.ModelViewSet):
 class BillingItemViewSet(viewsets.ModelViewSet):
     queryset = BillingItem.objects.all()
     serializer_class = BillingItemSerializer
+
+class BDDashBoardView(APIView):
+    permission_classes = [permissions.IsAuthenticated, IsBillingDesk]
+
+    def get(self, request, *args, **kwargs):
+        today = timezone.now().date()
+        today_bills = Billing.objects.filter(date__date=today)
+        total_by_payment_mode = today_bills.values('payment_mode').annotate(total_amount=Sum('total_amount'))
+        
+        serializer = BDDashBoardSerializer(total_by_payment_mode, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class GetCustomerByPhoneNumber(APIView):
+    def get(self, request, phone_number):
+        try:
+            customer = Customer.objects.filter(phone_number__icontains=phone_number)
+            serializer = CustomerSerializer(customer, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Customer.DoesNotExist:
+            return Response({'error': 'Customer not found'}, status=status.HTTP_404_NOT_FOUND)
